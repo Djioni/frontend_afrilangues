a46f4660d307fbbd730112e108473ad5466faec7
Mpongo Anganda <djioni@djionidev.com>
2025-11-24 16:02:16 +0100

    adding the API

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 /** @format */
-export const API_URL = "https://apis.africalangues.com/api";
+export const API_URL = import.meta.env.VITE_API_URL;
 export const AUTH_NAME = "Bearer";
 export const GOOGLE_CAPTCHA_SITE_LINK = "https://www.google.com/recaptcha/api/siteverify";
 export const GOOGLE_CAPTCHA_SITE_KEY = "6LfBhFwgAAAAAAsbcFFIpUGw5ywU54wh9fqHmOrO";
